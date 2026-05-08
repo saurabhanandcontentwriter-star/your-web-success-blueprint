8a95e14175f2f8d970f241e22dc0b94b45afd258
Saurabh Anand <saurabhanandcontentwriter@gmail.com>
2026-05-08 12:04:13 +0530

    Update SkillsSection.tsx

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -7,7 +7,7 @@ const skillGroups = [
   },
   {
     title: "Digital Marketing",
-    skills: ["Content Strategy", "Guest Blogging", "Google Analytics", "Search Console", "Marketing Strategy,"Google Ads",]
+    skills: ["Guest Blogging", "Google Analytics", "Search Console", "Marketing Strategy,"Google Ads"],
   },
   {
     title: "Design & UX",
